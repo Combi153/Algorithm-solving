23dc600168c1a43f6325aa0e632d21d3e486c0e7
chanminmaxscaler <jcm04@naver.com>
2022-12-19 23:55:06 +0900

    Add new solution

---
+++ java/baekjoon/implementation/no5597_과제/AssignmentChecker.java
@@ -0,0 +1,51 @@
+package date1004_implementation.no5597_과제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class AssignmentChecker {
+
+    private static final int OMITTED_STUDENT_NUMBER = 2;
+    private final BufferedReader bufferedReader;
+    private final List<Integer> students;
+
+    public AssignmentChecker(BufferedReader bufferedReader, List<Integer> students) {
+        this.bufferedReader = bufferedReader;
+        this.students = students;
+    }
+
+    public void printOmittedStudent() throws IOException {
+        List<Integer> omittedStudents = checkOmittedStudent();
+        for (Integer omittedStudent : omittedStudents) {
+            System.out.println(omittedStudent);
+        }
+    }
+
+    private List<Integer> checkOmittedStudent() throws IOException {
+        List<Integer> omittedStudents = copy();
+        for (int i = 0; i < students.size() - OMITTED_STUDENT_NUMBER; i++) {
+            omittedStudents.remove(readSubmitter());
+        }
+        return omittedStudents;
+    }
+
+    private List<Integer> copy() {
+        return new ArrayList<>(students);
+    }
+
+    private Integer readSubmitter() throws IOException {
+        String submitterNumber = bufferedReader.readLine();
+        return Integer.valueOf(submitterNumber);
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> students = new ArrayList<>();
+        IntStream.rangeClosed(1, 30).forEach(students::add);
+        AssignmentChecker assignmentChecker = new AssignmentChecker(new BufferedReader(new InputStreamReader(System.in)), students);
+        assignmentChecker.printOmittedStudent();
+    }
+}
