584839039a52879969eb4518f91e662c211c8e9c
chanminmaxscaler <jcm04@naver.com>
2022-12-26 00:11:55 +0900

    refactor: MatrixCalculator 클래스를 리팩토링한다
    변경 전:
    매직넘버, 하드코드가 존재한다.
    출력 시 과정이 복잡하다.
    
    변경 후:
    매직넘버, 하드코드를 없애고, 상수를 활용한다.
    stream 연산을 통해 출력 메시지를 만든 후 출력하도록 수정한다.

--- java/baekjoon/implementation/no2738_행렬덧셈/MatrixCalculator.java
+++ java/baekjoon/implementation/no2738_행렬덧셈/MatrixCalculator.java
@@ -10,6 +10,10 @@ import java.util.stream.Collectors;
 
 public class MatrixCalculator {
 
+    private static final String DELIMITER = " ";
+    private static final String NEW_LINE = "\n";
+    private static final int ROW_INDEX = 0;
+    private static final int COLUMN_INDEX = 1;
     private final BufferedReader bufferedReader;
     private int rowSize;
     private int columnSize;
@@ -21,24 +25,16 @@ public class MatrixCalculator {
     public void printSummationResult() throws IOException {
         readSizes();
         List<List<Integer>> resultMatrix = addMatrix(readMatrix(rowSize), readMatrix(rowSize));
-        for (int i = 0; i < rowSize; i++) {
-            List<Integer> row = resultMatrix.get(i);
-            for (int j = 0; j < columnSize; j++) {
-                System.out.print(row.get(j));
-            }
-            if (i != rowSize - 1) {
-                System.out.println();
-            }
-        }
+        printMatrix(resultMatrix);
     }
 
     private void readSizes() throws IOException {
         String line = bufferedReader.readLine();
-        List<Integer> sizes = Arrays.stream(line.split(" "))
+        List<Integer> sizes = Arrays.stream(line.split(DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        this.rowSize = sizes.get(0);
-        this.columnSize = sizes.get(1);
+        this.rowSize = sizes.get(ROW_INDEX);
+        this.columnSize = sizes.get(COLUMN_INDEX);
     }
 
     private List<List<Integer>> readMatrix(int rowSize) throws IOException {
@@ -51,14 +47,14 @@ public class MatrixCalculator {
 
     private List<Integer> readRow() throws IOException {
         String line = bufferedReader.readLine();
-        return Arrays.stream(line.split(" "))
+        return Arrays.stream(line.split(DELIMITER))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
     private List<List<Integer>> addMatrix(List<List<Integer>> matrixA, List<List<Integer>> matrixB) {
         List<List<Integer>> newMatrix = new ArrayList<>();
-        for (int i = 0; i < matrixA.size(); i++) {
+        for (int i = 0; i < rowSize; i++) {
             List<Integer> newRow = addRow(matrixA.get(i), matrixB.get(i));
             newMatrix.add(newRow);
         }
@@ -67,11 +63,20 @@ public class MatrixCalculator {
 
     private List<Integer> addRow(List<Integer> rowOfA, List<Integer> rowOfB) {
         List<Integer> newRow = new ArrayList<>();
-        for (int i = 0; i < rowOfA.size(); i++) {
+        for (int i = 0; i < columnSize; i++) {
             int summation = rowOfA.get(i) + rowOfB.get(i);
             newRow.add(summation);
         }
         return newRow;
+    }
+
+    private void printMatrix(List<List<Integer>> resultMatrix) {
+        String message = resultMatrix.stream()
+                .map(row -> row.stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(DELIMITER)))
+                .collect(Collectors.joining(NEW_LINE));
+        System.out.println(message);
     }
 
     public static void main(String[] args) throws IOException {
