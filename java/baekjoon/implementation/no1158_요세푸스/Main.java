7155f66f0155b907c69293d6f22e61309679a759
Combi153 <jcm04@naver.com>
2023-04-09 17:54:52 +0900

    feat: no1158_요세푸스 문제 풀이 Queue 사용

--- java/baekjoon/implementation/no1158_요세푸스/Main.java
+++ java/baekjoon/implementation/no1158_요세푸스/Main.java
@@ -3,10 +3,10 @@ package baekjoon.implementation.no1158_요세푸스;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -18,28 +18,21 @@ public class Main {
         final int count = Integer.parseInt(line.get(0));
         final int order = Integer.parseInt(line.get(1));
 
-        final List<Integer> integers = new LinkedList<>();
+        final Queue<Integer> numbers = new LinkedList<>();
         for (int i = 1; i <= count; i++) {
-            integers.add(i);
+            numbers.offer(i);
         }
 
-        final List<String> results = new ArrayList<>();
-        int index = 0;
-        for (int i = 0; i < count; i++) {
-            int endInclusive = integers.size() - 1;
-            for (int j = 1; j < order; j++) {
-                if (index == endInclusive || endInclusive == 0) {
-                    index = 0;
-                } else if (index > endInclusive) {
-                    index = 1;
-                } else {
-                    index++;
-                }
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (numbers.size() != 1) {
+            for (int i = 0; i < order - 1; i++) {
+                numbers.offer(numbers.poll());
             }
-            results.add(String.valueOf(integers.get(index)));
-            integers.remove(index);
+            sb.append(numbers.poll()).append(", ");
         }
-        System.out.println(results.stream().collect(Collectors.joining(", ", "<", ">")));
+        sb.append(numbers.poll()).append(">");
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws IOException {
