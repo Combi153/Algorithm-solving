14dd570ac4b50dec7513e06f1d1cdb8288eef5a2
Combi153 <jcm04@naver.com>
2023-12-26 20:45:48 +0900

    feat: 프로그래머스 명예의전당1 - 우선순위큐 사용

--- java/programmers/Level1/명예의전당1/Solution.java
+++ java/programmers/Level1/명예의전당1/Solution.java
@@ -1,46 +1,36 @@
 package programmers.Level1.명예의전당1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int[] solution(int k, int[] score) {
-        List<Integer> gs = new ArrayList<>();
-        int[] ps = new int[score.length];
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        if (k > score.length) {
-            for (int i = 0; i < score.length; i++) {
-                gs.add(score[i]);
-                ps[i] = gs.stream().min(Integer::compareTo).get();
-            }
-            return ps;
-        }
-
-        for (int i = 0; i < k; i++) {
-            gs.add(score[i]);
-            ps[i] = gs.stream().min(Integer::compareTo).get();
-        }
-
-        for (int i = k; i < score.length; i++) {
-            int min = gs.stream().min(Integer::compareTo).get();
-            int sc = score[i];
-
-            if (min < sc) {
-                gs.remove((Integer) min);
-                gs.add(sc);
-                min = gs.stream().min(Integer::compareTo).get();
+        for (int i = 0; i < score.length; i++) {
+            int s = score[i];
+            if (pq.size() < k) {
+                pq.add(s);
+                answer[i] = pq.peek();
+                continue;
             }
 
-            ps[i] = min;
+            if (pq.peek() < s) {
+                pq.add(s);
+                pq.remove();
+                answer[i] = pq.peek();
+            } else {
+                answer[i] = pq.peek();
+            }
         }
 
-        return ps;
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] result = solution.solution(78, new int[]{0, 0, 0, 0, 0, 0, 0});
+        int[] result = solution.solution(3, new int[]{10, 100, 20, 150, 1, 100, 200});
         System.out.println("result = " + Arrays.toString(result));
     }
 }
