699c8f02ecf1baa6526c560e7a5c4c51a89c16a2
Combi153 <jcm04@naver.com>
2024-03-11 14:58:13 +0900

    feat: 백준 no7785 hash set

--- java/baekjoon/no7785/Main.java
+++ java/baekjoon/no7785/Main.java
@@ -4,32 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<String, Boolean> log = new HashMap<>();
+        Set<String> nameLog = new HashSet<>();
 
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
-            Boolean isEntered = Objects.equals(st.nextToken(), "enter");
+            boolean isEntered = Objects.equals(st.nextToken(), "enter");
 
-            if (log.containsKey(name) && log.get(name) == isEntered) {
+            if (nameLog.contains(name) && !isEntered) {
+                nameLog.remove(name);
                 continue;
             }
-            log.put(name, isEntered);
+
+            if (isEntered) {
+                nameLog.add(name);
+            }
         }
 
-        log.keySet().stream()
-                .filter(log::get)
+        nameLog.stream()
                 .sorted(Comparator.reverseOrder())
                 .forEach(System.out::println);
 
