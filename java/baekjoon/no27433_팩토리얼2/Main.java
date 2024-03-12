357a814d3e786cbec6fcc7d9dd50cfb57d3d7f00
Combi153 <jcm04@naver.com>
2024-03-12 10:47:26 +0900

    feat: 백준 no27433 recursive fun

---
+++ java/baekjoon/no27433_팩토리얼2/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.no27433_팩토리얼2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static long factorial(long n) {
+        if (n < 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        System.out.println(factorial(n));
+
+        br.close();
+    }
+}
