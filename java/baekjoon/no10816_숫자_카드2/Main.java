cd3d4afc375bcb39fcf5c272a26334bbe5f28875
Combi153 <jcm04@naver.com>
2024-05-04 10:34:30 +0900

    feat: no10816 숫자 카드2_binarySearch

--- java/baekjoon/no10816_숫자_카드2/Main.java
+++ java/baekjoon/no10816_숫자_카드2/Main.java
@@ -3,13 +3,48 @@ package baekjoon.no10816_숫자_카드2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static Map<Integer, Integer> nByCount = new HashMap<>();
+    static List<Integer> numbers = new ArrayList<>();
+
+    static int upperBound(int number) {
+        int start = 0;
+        int end = numbers.size();
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int midNum = numbers.get(mid);
+
+            if (number < midNum) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+
+    static int lowerBound(int number) {
+        int start = 0;
+        int end = numbers.size();
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int midNum = numbers.get(mid);
+
+            if (number > midNum) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,19 +55,19 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             int number = Integer.parseInt(st.nextToken());
-            if (nByCount.containsKey(number)) {
-                nByCount.put(number, nByCount.get(number) + 1);
-            } else {
-                nByCount.put(number, 1);
-            }
+            numbers.add(number);
         }
+        numbers.sort(Comparator.naturalOrder());
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
             int mNum = Integer.parseInt(st.nextToken());
 
-            sb.append(nByCount.getOrDefault(mNum, 0)).append(" ");
+            int upperBound = upperBound(mNum);
+            int lowerBound = lowerBound(mNum);
+
+            sb.append(upperBound - lowerBound).append(" ");
         }
         System.out.println(sb);
     }
