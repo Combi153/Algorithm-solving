aaac9236c60615a745c9504426d9fe5b5ebdafa7
Combi153 <jcm04@naver.com>
2025-11-11 21:18:47 +0900

    feat: leetcode best time to buy and sell stock v2

--- java/leetcode/best_time_to_buy_and_sell_stock/Solution.java
+++ java/leetcode/best_time_to_buy_and_sell_stock/Solution.java
@@ -2,18 +2,24 @@ package leetcode.best_time_to_buy_and_sell_stock;
 
 public class Solution {
     public int maxProfit(int[] prices) {
+        int buyDay = 0;
+        int sellDay = 1;
         int maxProfit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                int buyPrice = prices[i];
-                int sellPrice = prices[j];
-                int profit = sellPrice - buyPrice;
 
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
+        while (sellDay < prices.length) {
+            int buyPrice = prices[buyDay];
+            int sellPrice = prices[sellDay];
+            int profit = sellPrice - buyPrice;
+            if (profit < 0) {
+                buyDay += 1;
+            } else if (profit > 0 && profit > maxProfit) {
+                maxProfit = profit;
+                sellDay += 1;
+            } else {
+                sellDay += 1;
             }
         }
         return maxProfit;
     }
+
 }
